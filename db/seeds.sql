4cde6d8394d0697d4258e4ba1ac18778e6cd5e83
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-08-28 00:57:23 -0700

    image not loading

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Pretzel Bun Barbeque Burger');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter and Jelly Burger');
