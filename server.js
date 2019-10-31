eb17829600fff02ef663325ab2490baa4b0dce6f
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-10-31 10:53:56 -0700

    working locally but not on heroku

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-
-var port = process.env.PORT || 3000;
-
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -17,8 +16,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgers_controller");
 
-app.use("/", routes);
+app.use(routes);
 
-app.listen(port);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
