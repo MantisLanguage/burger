db8a2608e665611ce5829f4e749ae7a3847306b0
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-10-31 10:59:35 -0700

    port glitch testing

--- server.js
+++ server.js
@@ -21,7 +21,5 @@ var routes = require("./controllers/burgers_controller");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().PORT, app.settings.env)});
